2a0925fcecb8ec862afdd6f1d2943e6434fcf7e8
Mandi Walls <mandi.walls@gmail.com>
2011-05-20 23:47:26 -0500

    cleaning up a bit, adding more robust matching
    * using the chef ui api to print messages in color. thanks @kallistec
    * changed straight matching to a regex so can match a substring or the whole output
    * delete the written-out file at the end - still working on stdout
    * include the number of hosts that successfully matched to the end of the output

--- canon.rb
+++ canon.rb
@@ -9,6 +9,7 @@ module Lnxchk
       require 'chef/knife/search'
       require 'chef/knife/ssh'
       require 'net/ssh/multi'
+      require 'chef/knife/core/ui'
     end
 
     banner "knife cannon -C \"good copy\" HOSTS_QUERY COMMAND"
@@ -33,8 +34,6 @@ module Lnxchk
       knife_ssh = Chef::Knife::Ssh.new
 
       cmd_line = command
-      knife_ssh.config[:no_color] = true
-      knife_ssh.config[:color] = false
       knife_ssh.name_args = [query, cmd_line]
       stdout_orig = $stdout
       $stdout = File.new('file', 'w')
@@ -42,14 +41,25 @@ module Lnxchk
       $stdout.close
       $stdout = stdout_orig
 
+      matches = 0
+
       file = File.open('./file', 'r')
       file.each_line do |line|
         host, data = line.split(" ")
-        if data != goodcopy
-          puts "#{host} failed to match expected output: #{data}"
+        if data !~ /#{goodcopy}/
+          ui.msg(ui.color("ERROR: #{host} failed to match expected output: #{data}", :red) )
+        else
+          matches = matches + 1
         end
       end
-      
+      file.close
+      File.delete('file')
+      if matches == 1
+        match_message = "1 host was successful"
+      else 
+        match_message = "#{matches} hosts were successful"
+      end
+      ui.msg(ui.color(match_message, :green))  
     end # close run
   end # close class
 end # close module
